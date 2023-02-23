9db72ee79aef14d1c17e56eec1fcd9120cfc6604
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-02-23 16:16:30 +0100

    Search bar
    Found this from Gijs!

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
+const searchbar = document.querySelector(".searchbar");
 
+searchbar.addEventListener("input", searching);
+
+function searching() {
+  let input = this.value;
+  input = input.toLowerCase();
+
+  let x = document.getElementsByClassName("members");
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "flex";
+    }
+    console.log(x[i].hidden);
+  }
+  console.log(this.value);
+}
